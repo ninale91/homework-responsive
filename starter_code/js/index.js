4b60475c0d21b6c16929f8c12830c2fb57085b40
ninale91 <ninale@Ninas-MacBook-Air.local>
2016-10-02 22:54:08 +1100

    formatting of categories

--- starter_code/js/index.js
+++ starter_code/js/index.js
@@ -2,8 +2,13 @@ $(document).ready(start);
 
 function start(){
   $('#hamburger-button').click(openMenu);
+  $('#plus-button').click(openCategory);
 }
 
 function openMenu() {
   $('nav').slideToggle();
+}
+
+function openCategory() {
+alert('working');
 }
